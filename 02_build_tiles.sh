83a8a9a50a0596b5092e3a814e77470a5ad8b2ae
Sergej Orlov <wladimirych@gmail.com>
2015-10-29 00:57:27 +0300

    fill map with solid color from zoom 4

--- 02_build_tiles.sh
+++ 02_build_tiles.sh
@@ -4,4 +4,4 @@ set -e
 
 [ -e "$MBTILES_FILE" ] && rm "$MBTILES_FILE"
 python plt2json.py $VMAPS_DIR/BRD/*.plt > $BORDER
-nice python make_tiles.py --vmap $VMAPS_DIR/vmap --out $MBTILES_FILE --format mbtiles --rscale 50000 --max-level 14 --metatile-level 9 --border $BORDER
+nice python make_tiles.py --vmap $VMAPS_DIR/vmap --out $MBTILES_FILE --format mbtiles --rscale 50000 --max-level 14 --metatile-level 9 --border $BORDER --highlight-level=4
